24c86b715b711d566bb0db9d3b090cbd8410e195
Tahauddin1990 <syedsdroid@gmail.com>
2024-02-12 18:22:36 +0300

    Point Cut Expressions

--- src/main/java/com/tahauddin/syed/aspect/BooksLoggingAspect.java
+++ src/main/java/com/tahauddin/syed/aspect/BooksLoggingAspect.java
@@ -1,9 +1,9 @@
 package com.tahauddin.syed.aspect;
 
-import com.tahauddin.syed.domain.entity.BooksEntity;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,11 +11,19 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class BooksLoggingAspect {
 
-    @Before("execution(com.tahauddin.syed.domain.entity.BooksEntity saveBook(com.tahauddin.syed.domain.entity.BooksEntity))")
-    public void beforeAddBooksEntity() {
-      log.info("In Aspect Before Adding Books");
+
+    @Pointcut("execution(com.tahauddin.syed.domain.entity.BooksEntity saveBook(com.tahauddin.syed.domain.entity.BooksEntity))")
+    public void pointCutExpression() { }
+
+    @Before("pointCutExpression()")
+    public void beforeAddBooksEntityOne() {
+        log.info("In Aspect Before Adding Books One");
     }
 
+    @Before("pointCutExpression()")
+    public void beforeAddBooksEntityTwo() {
+        log.info("In Aspect Before Adding Books Two");
+    }
 
 
 }
