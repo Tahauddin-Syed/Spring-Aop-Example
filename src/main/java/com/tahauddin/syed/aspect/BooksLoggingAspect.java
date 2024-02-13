7fe6f9140317b2aa9e316ecd100975c7c544a65b
Tahauddin1990 <syedsdroid@gmail.com>
2024-02-13 11:31:29 +0300

    Added Joint Point Code to get the method parameters

--- src/main/java/com/tahauddin/syed/aspect/BooksLoggingAspect.java
+++ src/main/java/com/tahauddin/syed/aspect/BooksLoggingAspect.java
@@ -1,10 +1,15 @@
 package com.tahauddin.syed.aspect;
 
+import com.tahauddin.syed.domain.entity.BooksEntity;
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.Signature;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
 
 @Component
 @Aspect
@@ -13,10 +18,14 @@ import org.springframework.stereotype.Component;
 public class BooksLoggingAspect {
 
     @Before("execution(com.tahauddin.syed.domain.entity.BooksEntity saveBook(com.tahauddin.syed.domain.entity.BooksEntity))")
-    public void beforeAddBooksEntity() {
-      log.info("In Books Logging Aspect --  Before Adding Books");
+    public void beforeAddBooksEntity(JoinPoint joinPoint) {
+        log.info("In Books Logging Aspect --  Before Adding Books");
+        Signature signature = joinPoint.getSignature();
+        log.info("Method Signature :: {}", signature);
+        Arrays.stream(joinPoint.getArgs())
+                .filter(BooksEntity.class :: isInstance)
+                .forEach(l -> log.info("Books Entity :: {}", l));
     }
-
 
 
 }
